a72634dc5eb08ca50ded99d57f386261b11efd0a
2miles <miles4599@gmail.com>
2021-01-17 00:58:01 -0800

    tilemap

--- src/Screen.java
+++ src/Screen.java
@@ -1,18 +1,23 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Screen {
 
     private int width, height;
     public int[] pixels;
+    public int[] tiles = new int[64 * 64];
 
-    int xtime = 0;
-    int ytime = 50;
-    int counter = 0;
+    private Random random = new Random();
 
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels = new int[width * height];
+
+        for(int i = 0; i < 64 * 64; ++i) {
+            tiles[i] = random.nextInt(0xffffff);
+        }
+
     }
 
     //set every pixel to black
@@ -22,14 +27,13 @@ public class Screen {
         }
     }
     public void render() {
-        ++counter;
-        if (counter % 100 == 0) ++xtime;
-        if (counter % 800 == 0) ++ytime;
+
         for (int y = 0; y < height; y++) {
-            if (ytime < 0 || ytime >= height) break;
+            if (y < 0 || y >= height) break;
             for (int x = 0; x < width; x++) {
-                if (xtime < 0 || xtime >= width) break;
-                pixels[xtime + ytime * width] = 0xff00ff;
+                if (x < 0 || x >= width) break;
+                int tileIndex = (x / 16) + (y / 16) * 64;
+                pixels[x + y * width] = tiles[tileIndex];
             }
         }
     }
