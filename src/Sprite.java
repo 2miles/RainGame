58568f75120817d575ce0f811f48b934a8d8739c
2miles <miles4599@gmail.com>
2021-01-18 19:05:14 -0800

    made a sprite sheet, and a sprite class

---
+++ src/Sprite.java
@@ -0,0 +1,29 @@
+
+
+public class Sprite {
+    public final int SIZE;
+    private int x, y;
+    public int[] pixels;
+    private SpriteSheet sheet;
+
+    public static Sprite grass = new Sprite(16, 0,0, null);
+
+    public Sprite(int size, int x, int y, SpriteSheet sheet) {
+        SIZE = size;
+        pixels = new int [SIZE * SIZE];
+        this.x = x * size;
+        this.y = y * size;
+        this.sheet = sheet;
+        load();
+    }
+
+    //extracting a single sprite out of our sprite sheet
+    //accessing the entire sprite sheet by pixel and
+    private void load() {
+        for (int y = 0; y < SIZE; ++y) {
+            for (int x = 0; x < SIZE; ++x) {
+                pixels[x + y * SIZE] = sheet.pixels[ (x + this.x) + (y + this.y) * sheet.SIZE];
+            }
+        }
+    }
+}
---
+++ src/SpriteSheet.java
@@ -0,0 +1,31 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+//in charge of managing any sprite sheets we might have
+//and cacheing them to memory
+public class SpriteSheet {
+    private String path; //system path to our sprite sheet
+    public final int SIZE;
+    public int[] pixels;
+
+    public SpriteSheet(String path, int size) {
+        this.path = path;
+        SIZE = size;
+        pixels = new int[SIZE * SIZE];
+        load();
+    }
+
+    private void load() {
+        try {
+            BufferedImage  image = ImageIO.read(SpriteSheet.class.getResource(path));
+            int w = image.getWidth();
+            int h = image.getHeight();
+            image.getRGB(0,0,w,h,pixels,0,w);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
