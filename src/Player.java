cd207db8362d7c75ce1198afc27a2298b1ffe177
2miles <miles4599@gmail.com>
2021-01-24 18:34:48 -0800

    Add all directioal sprites to player movement

--- src/Player.java
+++ src/Player.java
@@ -1,40 +1,37 @@
 public class Player extends Mob {
 
     private Keyboard input;
+    private Sprite sprite;
 
 
     public Player(Keyboard input) {
         this.input = input;
+        sprite = Sprite.player_forward;
     }
 
     public Player(int x, int y, Keyboard input) {
         this.x = x;
         this.y = y;
         this.input = input;
+        sprite = Sprite.player_forward;
     }
 
     public void update() {
         int xa = 0;
         int ya = 0;
+
         if (input.up) --ya;
         if (input.down) ++ya;
         if (input.left) --xa;
         if (input.right) ++xa;
-
         if (xa != 0 || ya != 0) move(xa, ya);
     }
 
     public void render(Screen screen) {
-
-        int xx = x - 16;
-        int yy = y - 16;
-
-
-        screen.renderPlayer(xx, yy, Sprite.player0);
-        screen.renderPlayer( xx + 16, yy, Sprite.player1);
-        screen.renderPlayer(xx, yy + 16, Sprite.player2);
-        screen.renderPlayer(xx + 16, yy + 16, Sprite.player3);
-
-
+        if (dir == 0) sprite = Sprite.player_forward;
+        if (dir == 1) sprite = Sprite.player_right;
+        if (dir == 2) sprite = Sprite.player_back;
+        if (dir == 3) sprite = Sprite.player_left;
+        screen.renderPlayer(x - 16, y - 16, sprite);
     }
 }
